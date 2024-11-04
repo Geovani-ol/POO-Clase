35e70fa169ba4460700cd8c8cb101822242db609
Geovani_Dev <davidolvera475@gmail.com>
2024-11-04 08:51:33 -0600

    Clase 04/11/2024

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
-import java.time.LocalDate;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        GestorSeguros gestorSeguros = new GestorSeguros();
-        gestorSeguros.seguros();
+        try {
+            int n = 0;
+            int m = 0;
+            int numero = n/m;
+            System.out.println("Resultado: " + numero);
+        } catch (Exception e) {
+            System.out.println("No se puede String a int: " + e);
+        } finally {
+            int a = 2, b = 9;
+            int suma = a + b;
+            System.out.println("Resultado de la suma: " + suma);
+        }
 
     }
 
